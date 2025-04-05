8c93aae1e05a87646875fd4fba4eb6c503ba56b0
nipuna1989 <jayjay0109@gmail.com>
2025-04-05 08:35:49 +0200

    Fixtures for page objects

--- conftest.py
+++ conftest.py
@@ -1,5 +1,9 @@
 import pytest
 import os
+
+from pages.esim_selection_page import EsimSelectionPage
+from pages.home_page import HomePage
+from pages.package_details_popup import PackageDetailsPopup
 from utils.logger import log_file_path
 from utils.get_token import get_access_token
 from playwright.sync_api import sync_playwright
@@ -24,6 +28,24 @@ def page(browser):
     page = context.new_page()
     yield page
     page.close()
+
+
+@pytest.fixture
+def home_page(page):
+    """Initialize Airalo home page objects and methods."""
+    return HomePage(page)
+
+
+@pytest.fixture
+def esim_selection_page(page):
+    """Initialize Airalo search page objects and methods."""
+    return EsimSelectionPage(page)
+
+
+@pytest.fixture
+def package_details_popup(page):
+    """Initialize Airalo package detail page objects and methods."""
+    return PackageDetailsPopup(page)
 
 
 @pytest.fixture(scope="module")
--- tests/ui/test_search_destination.py
+++ tests/ui/test_search_destination.py
@@ -1,26 +1,29 @@
-from pages.home_page import HomePage
-from pages.esim_selection_page import EsimSelectionPage
-from pages.package_details_popup import PackageDetailsPopup
 from data.load_test_data import TEST_DATA
 
 
-def test_search_destination(page):
+def test_search_destination(page, home_page, esim_selection_page, package_details_popup):
     """
     IMPORTANT: This test script has been created specifically for testing the functionality with the Euro currency.
-    Test to search for a destination in the search box and verify navigation to the correct page.
+
+    Steps:
+    1. Navigate to the homepage and search for a destination.
+    2. Select an eSIM package from the search results.
+    3. Verify the package details popup.
+
+    :param page: Playwright page object.
+    :param home_page: Fixture returning the HomePage object.
+    :param esim_selection_page: Fixture returning the EsimSelectionPage object.
+    :param package_details_popup: Fixture returning the PackageDetailsPopup object.
     """
     destination = TEST_DATA["search_destination"]
     expected_details = TEST_DATA["package_details"]
 
     # Step 1: Go to homepage and search
-    home_page = HomePage(page)
     home_page.navigate_to_home_page()
     home_page.search_for_destination(destination)
 
     # Step 2: Select eSIM
-    esim_selection_page = EsimSelectionPage(page)
     esim_selection_page.click_buy_now(2)
 
     # Step 3: Verify package details
-    package_details_popup = PackageDetailsPopup(page)
     package_details_popup.verify_package_details(expected_details)
